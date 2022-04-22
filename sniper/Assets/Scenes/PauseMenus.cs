30b3568da2b32ca5a95526f0b23424ae11413a59
Mukundh Vasudevan <58870019+PZES@users.noreply.github.com>
2022-04-22 17:54:59 -0400

    win and lose screen

--- sniper/Assets/Scenes/PauseMenus.cs
+++ sniper/Assets/Scenes/PauseMenus.cs
@@ -8,6 +8,8 @@ public class PauseMenus : MonoBehaviour
     // Start is called before the first frame update
 
     public GameObject pauseMenuUI;
+    public GameObject winUI;
+    public GameObject loseUI;
 
 
     public void Resume()
@@ -29,7 +31,34 @@ public class PauseMenus : MonoBehaviour
         Application.Quit();
     }
     // Update is called once per frame
+    public void Win(int points)
+    {
+        Time.timeScale = 0f;
+        winUI.SetActive(true);
+        int level = PlayerPrefs.GetInt("level")+1;
+        points += PlayerPrefs.GetInt("money");
+        PlayerPrefs.SetInt("level", level);
+        PlayerPrefs.SetInt("money", points);
+        PlayerPrefs.Save();
+    }
 
+    public void Lose()
+    {
+        Time.timeScale = 0f;
+        loseUI.SetActive(true);
+    }
 
+    public void nextLevel()
+    {
+        Time.timeScale = 1f;
+        winUI.SetActive(false);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
 
+    public void restart()
+    {
+        Time.timeScale = 1f;
+        loseUI.SetActive(false);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
 }
