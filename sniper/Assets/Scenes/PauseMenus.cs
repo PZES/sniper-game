6c14d902b8aa4ccdbf0e5ec6757b5c2df182d1ec
Mukundh Vasudevan <58870019+PZES@users.noreply.github.com>
2022-04-22 17:13:51 -0400

    menu good

--- sniper/Assets/Scenes/PauseMenus.cs
+++ sniper/Assets/Scenes/PauseMenus.cs
@@ -18,11 +18,13 @@ public class PauseMenus : MonoBehaviour
 
     public void Menu()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene(0);
     }
 
     public void Quit()
     {
+        Time.timeScale = 1f;
         Debug.Log("Quit");
         Application.Quit();
     }
